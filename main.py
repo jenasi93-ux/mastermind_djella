7bf2c4893740ea9ee6dc7da7e954f8eefe342c4b
djella oughou <djellaoughou@MacBook-Air-de-djella.local>
2025-10-24 16:45:35 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+def mastermind_creation():
+
+    list = ['Bleu', 'Vert', 'Jaune', 'Rouge', 'Blanc', 'Noir']
+    jeu_set = {}
+    while index in jeu_set < 4:
+
+        jeu_set = {random.choice(list)}
+
+        print(jeu_set)
+
+mastermind_creation()
